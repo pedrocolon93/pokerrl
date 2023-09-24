8dc05bf96c9d7f014555754e57e6261120247f5f
Pedro <pe25171@mit.edu>
2023-09-24 11:36:10 -0700

    Incorporated Treys library for evaluation of hands.

--- game.py
+++ game.py
@@ -1,42 +1,91 @@
+from enum import Enum
+from typing import *
+from treys import *
 
-class Game:
-    deck:[Card] = []
-    players:[Player] = []
-    public_cards:[Card] = []
-    hand:Int = 0
 
-    def setup_game(self, player_count = 1):
-        pass
 
-    def start_hand(self):
-        pass
-
-    def game_loop(self):
-        pass
-
-class Action(Enum):
-    START = 0
-    CALL = 1
-    RAISE = 2
-    RE_RAISE = 3
-    FOLD = 4
-    CHECK = 5
-    END = 6
 
 class Player:
+    class Action(Enum):
+        START = 0
+        CALL = 1
+        RAISE = 2
+        RE_RAISE = 3
+        FOLD = 4
+        CHECK = 5
+        END = 6
     hand:[Card] = []
-    score:Int = 0
-    playing:Bool = False
+    score:int = 0
+    playing:bool = False
     player_history:[Action]
-    def eval_turn(self, public_cards:[Card], other_players:[Player]):
+    def eval_turn(self, public_cards:[Card], other_players):
         pass
 
+class Game:
+
+    deck = Deck()
+    players:[Player] = []
+    public_cards:[Card] = []
+    hand:int = 0
+    evaluator = Evaluator()
+
+    def setup_game(self, player_count = 1):
+        self.state = Game.State.START
+        self.deck.shuffle()
+        self.public_cards = self.deck.draw(3)
+        for player_number in range(player_count):
+            player = Player()
+            player.hand = self.deck.draw(2)
+
+    class State(Enum):
+        START = 0
+        END = -1
+        FLOP = 1
+        TURN = 2
+        RIVER = 3
+
+    state = State.START
+    #TODO new hand function that shuffles players
+
+    def game_loop(self):
+        while self.state != Game.State.END:
+            if self.state == Game.State.START:
+                for player in self.players:
+                    action = player.eval_turn()
+
+        for player in self.players:
+            player_score = self.evaluator.evaluate(self.public_cards, player.hand)
+        self.hand+=1
+
+
+#
 class Suit(Enum):
     SPADES = 1
-    DIAMONDS = 2
-    CLOVER = 3
-    HEARTS = 4
+    HEARTS = 2
+    DIAMONDS = 3
+    CLUBS = 4
 
-class Card:
-    rank:Int
-    suit:Suit
+SuitMap = {
+    1:Suit.SPADES,
+    2:Suit.HEARTS,
+    4:Suit.DIAMONDS,
+    8:Suit.CLUBS
+}
+
+#
+class Cardy:
+    _card = None
+    rank:int
+    suit:int
+    def __init__(potato, card:Card):
+        potato.rank = Card.get_rank_int(card)
+        potato.suit = Card.get_suit_int(card)
+
+    def __str__(self):
+        return "Rank:"+str(self.rank)+" Suit:"+str(self.suit)
+
+if __name__ == '__main__':
+    c = Card.new("Ah")
+    ca = Cardy(c)
+    print(SuitMap[ca.suit])
+    print(ca)
